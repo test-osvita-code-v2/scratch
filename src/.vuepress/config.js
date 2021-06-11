e8be899e600e1ace8978811ec5ab5c03ca7a2eca
RenatTOP <renatseverski@gmail.com>
2021-06-11 21:46:58 +0300

    fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -140,28 +140,6 @@ module.exports = config({
                 type: "image/x-icon",
             },
         ],
-        [
-            "script",
-            {
-                src:
-                    "https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js",
-            },
-        ],
-        [
-            "script",
-            {
-                src:
-                    "https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js",
-            },
-        ],
-        ["script", { src: "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js" }],
-        [
-            "script",
-            {
-                src:
-                    "https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js",
-            },
-        ],
     ],
 
     themeConfig: {
