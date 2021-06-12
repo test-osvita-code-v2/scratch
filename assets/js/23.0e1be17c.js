425e89db7aed49e32ea649ce4eef6e7d426be945
RenatTOP <renatseverski@gmail.com>
2021-06-12 11:53:34 +0300

    deploy

---
+++ assets/js/23.0e1be17c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{488:function(e,t,n){"use strict";n.r(t);var r={name:"Redirect",props:{to:{type:String,required:!0}},beforeMount(){this.$router.push(this.to)}},i=n(1),o=Object(i.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=o.exports}}]);
